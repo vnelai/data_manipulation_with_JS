74a44d4e09221abd714c2ddc5f1d62aeb1d9f46d
vnelai <118484488+vnelai@users.noreply.github.com>
2024-11-04 10:06:12 -0500

    Check if all numbers are divisibkle by 5

--- Problem1.js
+++ Problem1.js
@@ -1,5 +1,7 @@
 // Data Manipulation with Javascript
 
+
+
 // The initial numbers that must be verified.
 const n1 = 10;
 const n2 = 15;
@@ -11,16 +13,23 @@ const n4 = 5;
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
 
+console.log("is sum 50: ", isSum50);
+
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 
+console.log("is two odd ", isTwoOdd);
+
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+
+
+console.log("is there a number over 25: ", isOver25);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -28,20 +37,21 @@ const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
+console.log("are all the numbers unique: ", isUnique);
+
+
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
-// Finally, log the results.
-console.log(isValid);
 
-// Here's another example of how this COULD be done,
-// but it SHOULD NOT be done this way. As programmers,
-// we break things into small, manageable pieces so that
-// they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
-  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+// Finally, log the results.
+console.log("is the code valid ", isValid);
+
+
+// Check if all numbers are divisible by 5
+ let divisible_By_Five = (n1%5==0) && (n2%5==0) && (n3%5==0) && (n4%5==0);
+
+console.log("is_all_divisible_by_five: ", divisible_By_Five);
